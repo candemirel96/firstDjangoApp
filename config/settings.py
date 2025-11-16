605916b6cab72a52e195317b05571cd05803467c
can <candemirel@gmail.com>
2025-11-16 04:36:09 +0300

    basic CRUD views created

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'firstdjango_db',
+        'USER': 'firstdjango_user',
+        'PASSWORD': 'firstdjango_password',
+        'HOST': '127.0.0.1',  # or 'localhost'
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
